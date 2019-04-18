6611d47eaf4abf833c43ea09d5bfa02c43fdb0f3
yurak <devnulldevnull@gmail.com>
2019-04-18 10:07:40 +0900

    add mysql

---
+++ mysql/init/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE webapp;
+use webapp;
+
+CREATE TABLE msg (
+  id int(5) unsigned not null auto_increment,
+  msg varchar(255) not null,
+  created_time datetime not null default current_timestamp,
+  updated_time datetime not null default current_timestamp on update current_timestamp,
+  primary key (id)
+);
