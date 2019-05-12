d525c9b697884f0c0d9563c91f5641f37c28dee2
yurak <devnulldevnull@gmail.com>
2019-05-12 19:22:32 +0900

    add insertdb.jsp

---
+++ src/main/java/web/InsertMessage.java
@@ -0,0 +1,53 @@
+package web;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import web.util.CreateId;
+import web.util.GetConfig;
+
+public class InsertMessage {
+
+	private static String jndiname = GetConfig.getResourceBundle("jndi.name");
+	private static String sqlkey = GetConfig.getResourceBundle("insert.msg.id");
+	private static String sqlbody = GetConfig.getResourceBundle("insert.msg.body");
+	private static String message = GetConfig.getResourceBundle("common.message");
+	private DataSource ds;
+
+	public String doGet() {
+		Connection con = null;
+		String id = String.valueOf(CreateId.createid());
+		String sql = GetConfig.getResourceBundle("insert.msg");
+
+		try {
+			sql = sql.replace(sqlkey, id);
+			sql = sql.replace(sqlbody, message);
+
+			InitialContext ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup(jndiname);
+			con = ds.getConnection();
+			Statement stmt = con.createStatement();
+
+			stmt.executeUpdate(sql);
+
+			System.out.println("Selected Msg: id: " + id + ", sql: " + sql);
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return "Insert DB";
+	}
+
+}
