0507ba51f42764d144434596c74b53cfeea3a40b
yurak <devnulldevnull@gmail.com>
2019-05-16 01:13:44 +0900

    add mq i/f

--- src/main/java/web/ApplicationController.java
+++ src/main/java/web/ApplicationController.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import web.cache.GetCache;
+import web.cache.SetCache;
 import web.db.DeleteMessage;
 import web.db.InsertMessage;
 import web.db.SelectMessage;
+import web.mq.GetMq;
+import web.mq.PutMq;
+import web.mq.PutMqBatch;
 
 @Controller
 public class ApplicationController {
@@ -19,11 +24,13 @@ public class ApplicationController {
 	}
 
 	@RequestMapping("InsertDB")
-	public String insertDb() {
+	public String insertDb(Model model) {
 
 		System.out.println("InsertDB");
 		InsertMessage insmsg = new InsertMessage();
-		insmsg.insertMsg();
+		String msg = insmsg.insertMsg();
+
+		model.addAttribute("insertMsg", msg);
 
 		return "insertdb";
 	}
@@ -36,7 +43,6 @@ public class ApplicationController {
 		List<String> allMessage = insmsg.selectMsg();
 
 		model.addAttribute("allMessageList", allMessage);
-
 		return "selectdb";
 	}
 
@@ -48,5 +54,65 @@ public class ApplicationController {
 		insmsg.deleteMsg();
 
 		return "deletedb";
+	}
+
+	@RequestMapping("GetMQ")
+	public String getMq(Model model) {
+
+		System.out.println("GetMQ");
+		GetMq getmq = new GetMq();
+		String telegram = getmq.getMessageQueue();
+
+		model.addAttribute("getMsgQueue", telegram);
+
+		return "getmq";
+	}
+
+	@RequestMapping("PutMQ")
+	public String putMq(Model model) {
+
+		System.out.println("PutMQ");
+		PutMq putmq = new PutMq();
+		String telegram = putmq.putMessageQueue();
+
+		model.addAttribute("putMsgQueue", telegram);
+
+		return "putmq";
+	}
+
+	@RequestMapping("PutMQBatch")
+	public String putMqBatch(Model model) {
+
+		System.out.println("PutMQBatch");
+		PutMqBatch putmqb = new PutMqBatch();
+		String telegram = putmqb.putMessageQueueBatch();
+
+		model.addAttribute("putMsgQueueBatch", telegram);
+
+		return "putmqbatch";
+	}
+
+	@RequestMapping("GetCache")
+	public String getCache(Model model) {
+
+		System.out.println("GetCache");
+		GetCache getcache = new GetCache();
+		String cache = getcache.getCache();
+
+		model.addAttribute("getCache", cache);
+
+		return "getcache";
+	}
+
+	@RequestMapping("SetCache")
+	public String setCache(Model model) {
+
+		System.out.println("PutCache");
+		SetCache setcache = new SetCache();
+		String cache = setcache.setCache();
+
+		model.addAttribute("setCache", cache);
+
+		return "setcache";
 	}
 }
