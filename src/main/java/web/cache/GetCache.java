9b412c58aee5435ab563ad2bc1b9ce9466739d5b
yurak <devnulldevnull@gmail.com>
2019-05-16 01:14:44 +0900

    add cache i/f

---
+++ src/main/java/web/cache/GetCache.java
@@ -0,0 +1,35 @@
+package web.cache;
+
+import javax.servlet.http.HttpServlet;
+
+import com.meetup.memcached.MemcachedClient;
+import com.meetup.memcached.SockIOPool;
+
+import web.util.GetConfig;
+
+public class GetCache extends HttpServlet {
+	private static String serverconf = GetConfig.getResourceBundle("cache.server.conf");
+
+	// コネクションプールの初期化
+	static {
+		SockIOPool pool = SockIOPool.getInstance();
+		pool.setServers(new String[] { serverconf });
+		pool.initialize();
+	}
+
+	public String getCache() {
+		String fullmsg = null;
+		MemcachedClient mcc = new MemcachedClient();
+
+		try {
+			String id = (String) mcc.get("id");
+			String message = (String) mcc.get("msg");
+
+			fullmsg = "Received id: " + id + ", msg: " + message;
+			System.out.println(fullmsg);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return fullmsg;
+	}
+}
---
+++ src/main/java/web/cache/SetCache.java
@@ -0,0 +1,38 @@
+package web.cache;
+
+import javax.servlet.http.HttpServlet;
+
+import com.meetup.memcached.MemcachedClient;
+import com.meetup.memcached.SockIOPool;
+
+import web.util.CreateId;
+import web.util.GetConfig;
+
+public class SetCache extends HttpServlet {
+	private static String serverconf = GetConfig.getResourceBundle("cache.server.conf");
+	private static String message = GetConfig.getResourceBundle("common.message");
+
+	// コネクションプールの初期化
+	static {
+		SockIOPool pool = SockIOPool.getInstance();
+		pool.setServers(new String[] { serverconf });
+		pool.initialize();
+	}
+
+	public String setCache() {
+		String fullmsg = null;
+		String id = String.valueOf(CreateId.createid());
+
+		MemcachedClient mcc = new MemcachedClient();
+		try {
+			mcc.set("id", id);
+			mcc.set("msg", message);
+
+			fullmsg = "Set id: " + id + ", msg: " + message;
+			System.out.println(fullmsg);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return fullmsg;
+	}
+}
