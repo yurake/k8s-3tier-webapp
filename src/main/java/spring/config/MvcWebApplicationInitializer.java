c61e1d697176ad59e442a5dab5c31227062e9dec
yurak <devnulldevnull@gmail.com>
2019-05-20 23:46:44 +0900

    change from xml to java

---
+++ src/main/java/spring/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { MvcWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
