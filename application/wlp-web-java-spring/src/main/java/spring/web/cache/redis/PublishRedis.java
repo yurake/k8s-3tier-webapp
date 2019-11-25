d5023c4e7aa23b67d46e97fb389060d2f36dea35
yurak <devnulldevnull21@gmail.com>
2019-11-25 23:06:28 +0900

    change publish channel

--- application/wlp-web-java-spring/src/main/java/spring/web/cache/redis/PublishRedis.java
+++ application/wlp-web-java-spring/src/main/java/spring/web/cache/redis/PublishRedis.java
@@ -9,6 +9,9 @@ public class PublishRedis {
 	private static String servername = GetConfig.getResourceBundle("redis.server.name");
 	private static int serverport= Integer.parseInt(GetConfig.getResourceBundle("redis.server.port"));
 	private static int setexpire = Integer.parseInt(GetConfig.getResourceBundle("redis.set.expire"));
+    private static String splitkey = GetConfig.getResourceBundle("redis.split.key");
+    private static String channel = GetConfig.getResourceBundle("redis.channel.name");
+
 
 	public String publishRedis() {
 		String fullmsg = null;
@@ -16,9 +19,15 @@ public class PublishRedis {
 		Jedis jedis = new Jedis(servername, serverport);
 
 		try {
-			jedis.publish(id, message);
+		    StringBuilder buf = new StringBuilder();
+		    buf.append(id);
+		    buf.append(splitkey);
+		    buf.append(message);
+		    String body = buf.toString();
+
+			jedis.publish(channel, body);
 			jedis.expire(id, setexpire);
-			fullmsg = "Set id: " + id + ", msg: " + message;
+			fullmsg = "Set channel:" + channel + ", id: " + id + ", msg: " + message;
 			System.out.println(fullmsg);
 		} catch (Exception e) {
 			e.printStackTrace();
