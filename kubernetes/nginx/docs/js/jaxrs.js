964d57f1dda2f8c0602a4b221d4c95ceb1d18e06
yurak <devnulldevnull21@gmail.com>
2019-12-30 00:03:08 +0900

    add button for resources

--- kubernetes/nginx/docs/js/jaxrs.js
+++ kubernetes/nginx/docs/js/jaxrs.js
@@ -1,13 +1,18 @@
 root = location.href;
-url = root + 'api/mysql';
+url = root + 'api';
+memcachedurl = url + '/memcached';
+redisurl = url + '/redis';
+rabbitmqurl = url + '/rabbitmq';
+mysqlurl = url + '/mysqlurl';
+quarkusurl = root + 'quarkus/mysql';
 
 $(function () {
 	$("#response").html("Response Values");
 
-	$("#insert").click(function () {
+	$("#setmemcached").click(function () {
 		$.ajax({
 			type: 'post',
-			url: url + '/insert',
+			url: memcachedurl + '/set',
 			contentType: 'application/json',
 			scriptCharset: 'utf-8',
 		}).done(function (data) {
@@ -21,10 +26,196 @@ $(function () {
 		})
 	})
 
-	$("#select").click(function () {
+	$("#getmemcached").click(function () {
 		$.ajax({
 			type: 'get',
-			url: url + '/select',
+			url: memcachedurl + '/get',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#setredis").click(function () {
+		$.ajax({
+			type: 'post',
+			url: redisurl + '/set',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#getredis").click(function () {
+		$.ajax({
+			type: 'get',
+			url: redisurl + '/get',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#publishredis").click(function () {
+		$.ajax({
+			type: 'post',
+			url: redisurl + '/publish',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#putrabbitmq").click(function () {
+		$.ajax({
+			type: 'post',
+			url: rabbitmqurl + '/put',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#getrabbitmq").click(function () {
+		$.ajax({
+			type: 'get',
+			url: rabbitmqurl + '/get',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#publishrabbitmq").click(function () {
+		$.ajax({
+			type: 'post',
+			url: rabbitmqurl + '/publish',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+	$("#insertmysql").click(function () {
+		$.ajax({
+			type: 'post',
+			url: mysqlurl + '/insert',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#selectmysql").click(function () {
+		$.ajax({
+			type: 'get',
+			url: mysqlurl + '/select',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#deletemysql").click(function () {
+		$.ajax({
+			type: 'post',
+			url: mysqlurl + '/delete',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#insertquarkus").click(function () {
+		$.ajax({
+			type: 'post',
+			url: quarkusurl + '/insert',
+			contentType: 'application/json',
+			scriptCharset: 'utf-8',
+		}).done(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		}).fail(function (data) {
+			const resp = JSON.stringify(data, null, 4)
+			console.log(resp);
+			$("#response").html(resp);
+		})
+	})
+
+	$("#selectquarkus").click(function () {
+		$.ajax({
+			type: 'get',
+			url: quarkusurl + '/select',
 			contentType: 'application/json',
 			scriptCharset: 'utf-8',
 		}).done(function (data) {
