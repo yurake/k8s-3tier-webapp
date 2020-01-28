ec2cd79e3807bbc2cdef2a8b79a2c70d77423dfc
yurak <devnulldevnull21@gmail.com>
2020-01-29 02:01:33 +0900

    show status icon

--- application/quarkus-build.sh
+++ application/quarkus-build.sh
@@ -3,14 +3,6 @@ set -eu
 
 ROOT_DIR=$(cd $(dirname -- $0); pwd)
 
-# echo "### webapp-service"
-# cd webapp-service
-# ./mvnw clean
-# ./mvnw package
-# cd ${ROOT_DIR}
-# echo "###"
-# echo ""
-
 echo "### jaxrs-quarkus"
 cd jaxrs-quarkus
 ./mvnw clean
