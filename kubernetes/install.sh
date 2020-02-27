5ed4db9b5ee268e71eef62f4ae946e8559c65b68
yurak <devnulldevnull21@gmail.com>
2020-02-28 01:48:09 +0900

    Update install.sh

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -6,207 +6,245 @@ ROOT_DIR=$(cd $(dirname -- $0); pwd)
 echo "## default"
 
 echo "### mysql"
-docker build -t default/mysql:v0.0.1 ./mysql/.
-kubectl apply -f ./mysql/mysql-pv.yaml
-kubectl apply -f ./mysql/mysql-pvc.yaml
-kubectl apply -f ./mysql/mysql-configmap.yaml
-kubectl apply -f ./mysql/mysql-secret.yaml
-kubectl apply -f ./mysql/mysql-deployment.yaml
-kubectl apply -f ./mysql/mysql-service.yaml
+cd mysql
+docker build -t default/mysql:v0.0.1 .
+kubectl apply -f ./mysql-pv.yaml
+kubectl apply -f ./mysql-pvc.yaml
+kubectl apply -f ./mysql-configmap.yaml
+kubectl apply -f ./mysql-secret.yaml
+kubectl apply -f ./mysql-deployment.yaml
+kubectl apply -f ./mysql-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### postgres"
-docker build -t default/postgres:v0.0.1 ./postgres/.
-kubectl apply -f ./postgres/postgres-pv.yaml
-kubectl apply -f ./postgres/postgres-pvc.yaml
-kubectl apply -f ./postgres/postgres-configmap.yaml
-kubectl apply -f ./postgres/postgres-secret.yaml
-kubectl apply -f ./postgres/postgres-deployment.yaml
-kubectl apply -f ./postgres/postgres-service.yaml
+cd postgres
+docker build -t default/postgres:v0.0.1 .
+kubectl apply -f ./postgres-pv.yaml
+kubectl apply -f ./postgres-pvc.yaml
+kubectl apply -f ./postgres-configmap.yaml
+kubectl apply -f ./postgres-secret.yaml
+kubectl apply -f ./postgres-deployment.yaml
+kubectl apply -f ./postgres-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### redis"
-kubectl apply -f ./redis/redis-deployment.yaml
-kubectl apply -f ./redis/redis-service.yaml
+cd redis
+kubectl apply -f ./redis-deployment.yaml
+kubectl apply -f ./redis-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### rabbitmq"
-docker build -t default/rabbitmq:v0.0.1 ./rabbitmq/.
-kubectl apply -f ./rabbitmq/rabbitmq-configmap.yaml
-kubectl apply -f ./rabbitmq/rabbitmq-secret.yaml
-kubectl apply -f ./rabbitmq/rabbitmq-deployment.yaml
-kubectl apply -f ./rabbitmq/rabbitmq-service.yaml
-kubectl apply -f ./rabbitmq/rabbitmq-ingress.yaml
+cd rabbitmq
+docker build -t default/rabbitmq:v0.0.1 .
+kubectl apply -f ./rabbitmq-configmap.yaml
+kubectl apply -f ./rabbitmq-secret.yaml
+kubectl apply -f ./rabbitmq-deployment.yaml
+kubectl apply -f ./rabbitmq-service.yaml
+kubectl apply -f ./rabbitmq-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### activemq"
-kubectl apply -f ./activemq/activemq-deployment.yaml
-kubectl apply -f ./activemq/activemq-service.yaml
-kubectl apply -f ./activemq/activemq-ingress.yaml
+cd activemq
+kubectl apply -f ./activemq-deployment.yaml
+kubectl apply -f ./activemq-service.yaml
+kubectl apply -f ./activemq-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### hazelcast"
-kubectl apply -f ./hazelcast/hazelcast-rbac.yaml
-kubectl apply -f ./hazelcast/hazelcast-configmap.yaml
-kubectl apply -f ./hazelcast/hazelcast-statefulset.yaml
-kubectl apply -f ./hazelcast/hazelcast-service.yaml
+cd hazelcast
+kubectl apply -f ./hazelcast-rbac.yaml
+kubectl apply -f ./hazelcast-configmap.yaml
+kubectl apply -f ./hazelcast-statefulset.yaml
+kubectl apply -f ./hazelcast-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### memcached"
-kubectl apply -f ./memcached/memcached-deployment.yaml
-kubectl apply -f ./memcached/memcached-service.yaml
+cd memcached
+kubectl apply -f ./memcached-deployment.yaml
+kubectl apply -f ./memcached-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### nginx"
-docker build -t default/nginx:v0.0.1 ./nginx/.
-kubectl apply -f ./nginx/nginx-deployment.yaml
-kubectl apply -f ./nginx/nginx-service.yaml
-kubectl apply -f ./nginx/nginx-ingress.yaml
+cd nginx
+docker build -t default/nginx:v0.0.1 .
+kubectl apply -f ./nginx-deployment.yaml
+kubectl apply -f ./nginx-service.yaml
+kubectl apply -f ./nginx-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### rabbitmq-mysql-quarkus"
 cd ../application/rabbitmq-mysql-quarkus
 docker build -t default/rabbitmq-mysql-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/rabbitmq-mysql-quarkus
+kubectl apply -f ./rabbitmq-mysql-quarkus-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./rabbitmq-mysql-quarkus/rabbitmq-mysql-quarkus-deployment.yaml
 echo "###"
 echo ""
 
 echo "### wlp"
-cp -p ../application/wlp-web-java-spring/target/spring.war ./wlp/.
-docker build -t default/wlp:v0.0.1 ./wlp/.
-kubectl apply -f ./wlp/wlp-configmap.yaml
-kubectl apply -f ./wlp/wlp-deployment.yaml
-kubectl apply -f ./wlp/wlp-service.yaml
-kubectl apply -f ./wlp/wlp-ingress.yaml
+cp -p ../application/wlp-web-java-spring/target/spring.war .
+cd ${ROOT_DIR}/wlp
+docker build -t default/wlp:v0.0.1 .
+kubectl apply -f ./wlp-configmap.yaml
+kubectl apply -f ./wlp-deployment.yaml
+kubectl apply -f ./wlp-service.yaml
+kubectl apply -f ./wlp-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### jaxrs-quarkus"
 cd ../application/jaxrs-quarkus
 docker build -t default/jaxrs-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/jaxrs-quarkus
+kubectl apply -f ./jaxrs-quarkus-deployment.yaml
+kubectl apply -f ./jaxrs-quarkus-service.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./jaxrs-quarkus/jaxrs-quarkus-deployment.yaml
-kubectl apply -f ./jaxrs-quarkus/jaxrs-quarkus-service.yaml
 echo "###"
 echo ""
 
 echo "### redis-mysql-quarkus"
 cd ../application/redis-mysql-quarkus
 docker build -t default/redis-mysql-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/redis-mysql-quarkus
+kubectl apply -f ./redis-mysql-quarkus-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./redis-mysql-quarkus/redis-mysql-quarkus-deployment.yaml
 echo "###"
 echo ""
 
 echo "### redis-mysql-helidon"
 cd ../application/redis-mysql-helidon
-# docker build -t default/redis-mysql-helidon:v0.0.1 .
+cd ${ROOT_DIR}/redis-mysql-helidon
+kubectl apply -f ./redis-mysql-helidon-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./redis-mysql-helidon/redis-mysql-helidon-deployment.yaml
 echo "###"
 echo ""
 
 echo "### hazelcast-mysql-quarkus"
 cd ../application/hazelcast-mysql-quarkus
 docker build -t default/hazelcast-mysql-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/hazelcast-mysql-quarkus
+kubectl apply -f ./hazelcast-mysql-quarkus-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./hazelcast-mysql-quarkus/hazelcast-mysql-quarkus-deployment.yaml
 echo "###"
 echo ""
 
 echo "### activemq-mysql-quarkus"
 cd ../application/activemq-mysql-quarkus
 docker build -t default/activemq-mysql-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/activemq-mysql-quarkus
+kubectl apply -f ./activemq-mysql-quarkus-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./activemq-mysql-quarkus/activemq-mysql-quarkus-deployment.yaml
 echo "###"
 echo ""
 
 echo "### randompublish-quarkus"
 cd ../application/randompublish-quarkus
 docker build -t default/randompublish-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/randompublish-quarkus
+kubectl apply -f ./randompublish-quarkus-deployment.yaml
+kubectl apply -f ./randompublish-quarkus-service.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./randompublish-quarkus/randompublish-quarkus-deployment.yaml
-kubectl apply -f ./randompublish-quarkus/randompublish-quarkus-service.yaml
 echo "###"
 echo ""
 
 echo "### scheduled-quarkus"
 cd ../application/scheduled-quarkus
 docker build -t default/scheduled-quarkus:v0.0.1 -f src/main/docker/Dockerfile.jvm .
+cd ${ROOT_DIR}/scheduled-quarkus
+kubectl apply -f ./scheduled-quarkus-deployment.yaml
 cd ${ROOT_DIR}
-kubectl apply -f ./scheduled-quarkus/scheduled-quarkus-deployment.yaml
 echo "###"
 echo ""
 
 echo "## monitoring"
-kubectl apply -f ./monitoring/monitoring-namespace.yaml
+cd ${ROOT_DIR}/monitoring
+kubectl apply -f ./monitoring-namespace.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### jenkins"
-docker build -t monitoring/jenkins:v0.0.1 ./monitoring/jenkins/.
-kubectl apply -f ./monitoring/jenkins/jenkins-rbac.yaml
-kubectl apply -f ./monitoring/jenkins/jenkins-pv.yaml
-kubectl apply -f ./monitoring/jenkins/jenkins-pvc.yaml
-kubectl apply -f ./monitoring/jenkins/jenkins-deployment.yaml
-kubectl apply -f ./monitoring/jenkins/jenkins-service.yaml
-kubectl apply -f ./monitoring/jenkins/jenkins-ingress.yaml
+cd ${ROOT_DIR}/monitoring/jenkins
+docker build -t monitoring/jenkins:v0.0.1 .
+kubectl apply -f ./jenkins-rbac.yaml
+kubectl apply -f ./jenkins-pv.yaml
+kubectl apply -f ./jenkins-pvc.yaml
+kubectl apply -f ./jenkins-deployment.yaml
+kubectl apply -f ./jenkins-service.yaml
+kubectl apply -f ./jenkins-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### node-exporter"
-kubectl apply -f ./monitoring/node-exporter/node-exporter-pv-sys.yaml
-kubectl apply -f ./monitoring/node-exporter/node-exporter-pvc-sys.yaml
-kubectl apply -f ./monitoring/node-exporter/node-exporter-pv-proc.yaml
-kubectl apply -f ./monitoring/node-exporter/node-exporter-pvc-proc.yaml
-kubectl apply -f ./monitoring/node-exporter/node-exporter-daemonset.yaml
-kubectl apply -f ./monitoring/node-exporter/node-exporter-service.yaml
+cd ${ROOT_DIR}/monitoring/node-exporter
+kubectl apply -f ./node-exporter-pvc-sys.yaml
+kubectl apply -f ./node-exporter-pv-proc.yaml
+kubectl apply -f ./node-exporter-pvc-proc.yaml
+kubectl apply -f ./node-exporter-daemonset.yaml
+kubectl apply -f ./node-exporter-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### kube-state-metrics"
-kubectl apply -f ./monitoring/kube-state-metrics/kube-state-metrics-rbac.yaml
-kubectl apply -f ./monitoring/kube-state-metrics/kube-state-metrics-deployment.yaml
-kubectl apply -f ./monitoring/kube-state-metrics/kube-state-metrics-service.yaml
+cd ${ROOT_DIR}/monitoring/kube-state-metrics
+kubectl apply -f ./kube-state-metrics-rbac.yaml
+kubectl apply -f ./kube-state-metrics-deployment.yaml
+kubectl apply -f ./kube-state-metrics-service.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### prometheus"
-kubectl apply -f ./monitoring/prometheus/prometheus-rbac.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-pv.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-configmap.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-statefulset.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-service.yaml
-kubectl apply -f ./monitoring/prometheus/prometheus-ingress.yaml
+cd ${ROOT_DIR}/monitoring/prometheus
+kubectl apply -f ./prometheus-rbac.yaml
+kubectl apply -f ./prometheus-pv.yaml
+kubectl apply -f ./prometheus-configmap.yaml
+kubectl apply -f ./prometheus-statefulset.yaml
+kubectl apply -f ./prometheus-service.yaml
+kubectl apply -f ./prometheus-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### grafana"
+cd ${ROOT_DIR}
 kubectl apply -f ./monitoring/grafana/grafana-pv.yaml
 kubectl apply -f ./monitoring/grafana/grafana-pvc.yaml
 kubectl apply -f ./monitoring/grafana/grafana-deployment.yaml
 kubectl apply -f ./monitoring/grafana/grafana-service.yaml
 kubectl apply -f ./monitoring/grafana/grafana-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
 echo "### jupyter"
+cd ${ROOT_DIR}
 kubectl apply -f ./monitoring/jupyter/jupyter-pv.yaml
 kubectl apply -f ./monitoring/jupyter/jupyter-pvc.yaml
 kubectl apply -f ./monitoring/jupyter/jupyter-configmap.yaml
 kubectl apply -f ./monitoring/jupyter/jupyter-deployment.yaml
 kubectl apply -f ./monitoring/jupyter/jupyter-service.yaml
 kubectl apply -f ./monitoring/jupyter/jupyter-ingress.yaml
+cd ${ROOT_DIR}
 echo "###"
 echo ""
 
