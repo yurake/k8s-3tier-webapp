cc8249ded58b96821596397160be7b8b3db168bd
yurak <devnulldevnull21@gmail.com>
2020-03-16 23:20:07 +0900

    remove private setting

--- kubernetes/test/test.sh
+++ kubernetes/test/test.sh
@@ -18,7 +18,7 @@ echo "#### rabbitmq"
 echo "#################"
 cd ab
 docker build -t test/ab:v0.0.1 .
-docker run --rm -d --name ab --network oss-3tier-webapp test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/rabbitmq/publish
+docker run --rm -d --name ab test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/rabbitmq/publish
 docker logs -f ab
 cd ${ROOT_DIR}
 echo ""
@@ -28,7 +28,7 @@ echo "#### activemq"
 echo "#################"
 cd ab
 docker build -t test/ab:v0.0.1 .
-docker run --rm -d --name ab --network oss-3tier-webapp test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/activemq/publish
+docker run --rm -d --name ab test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/activemq/publish
 docker logs -f ab
 cd ${ROOT_DIR}
 echo ""
@@ -38,7 +38,7 @@ echo "#### redis"
 echo "#################"
 cd ab
 docker build -t test/ab:v0.0.1 .
-docker run --rm -d --name ab --network oss-3tier-webapp test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/redis/publish
+docker run --rm -d --name ab test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/redis/publish
 docker logs -f ab
 cd ${ROOT_DIR}
 echo ""
@@ -48,7 +48,7 @@ echo "#### hazelcast"
 echo "#################"
 cd ab
 docker build -t test/ab:v0.0.1 .
-docker run --rm -d --name ab --network oss-3tier-webapp test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/hazelcast/publish
+docker run --rm -d --name ab test/ab:v0.0.1 -n 100 -c 100 -p ./sample.json -T "application/json; charset=utf-8" http://k8s.3tier.webapp/quarkus/hazelcast/publish
 docker logs -f ab
 cd ${ROOT_DIR}
 echo ""
