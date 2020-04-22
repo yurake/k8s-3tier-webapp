b3532fdc51f6720c0b03052f38a0b3c596028083
yurak <devnulldevnull21@gmail.com>
2020-04-23 01:50:13 +0900

    change readiness to liveness #114

--- application/jaxrs-postgres-quarkus/src/main/java/webapp/tier/healthcheck/LivenessHealthCheck.java
+++ application/jaxrs-postgres-quarkus/src/main/java/webapp/tier/healthcheck/LivenessHealthCheck.java
@@ -1,17 +1,33 @@
 package webapp.tier.healthcheck;
 
+import java.util.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 
+import webapp.tier.service.PostgresService;
+
 @Liveness
 @ApplicationScoped
 public class LivenessHealthCheck implements HealthCheck {
 
+	@Inject
+	PostgresService posgresvc;
+
+	private static final Logger LOG = Logger.getLogger(LivenessHealthCheck.class.getSimpleName());
+
 	@Override
 	public HealthCheckResponse call() {
-		return HealthCheckResponse.up("OK");
+		if (posgresvc.connectionStatus()) {
+			LOG.fine("LivenessHealthCheck: UP");
+			return HealthCheckResponse.up("Database connection health check");
+		} else {
+			LOG.warning("LivenessHealthCheck: DOWN");
+			return HealthCheckResponse.down("Database connection health check");
+		}
 	}
 }
--- application/jaxrs-postgres-quarkus/src/main/java/webapp/tier/healthcheck/ReadinessHealthCheck.java
+++ application/jaxrs-postgres-quarkus/src/main/java/webapp/tier/healthcheck/ReadinessHealthCheck.java
@@ -1,32 +1,18 @@
 package webapp.tier.healthcheck;
 
-import java.util.logging.Logger;
-
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
 
-import webapp.tier.service.PostgresService;
-
 @Readiness
 @ApplicationScoped
 public class ReadinessHealthCheck implements HealthCheck {
 
-	private static final Logger LOG = Logger.getLogger(ReadinessHealthCheck.class.getSimpleName());
-
 
 	@Override
 	public HealthCheckResponse call() {
-		PostgresService posgresvc = new PostgresService();
-
-		if (posgresvc.connectionStatus()) {
-			LOG.fine("Readiness: UP");
-			return HealthCheckResponse.up("Database connection health check");
-		} else {
-			LOG.warning("Readiness: DOWN");
-			return HealthCheckResponse.down("Database connection health check");
-		}
+		return HealthCheckResponse.up("OK");
 	}
 }
