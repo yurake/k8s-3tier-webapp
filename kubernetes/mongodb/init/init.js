550bda3116288641c421287d64a5f759727f1501
yurak <devnulldevnull21@gmail.com>
2020-04-25 00:20:12 +0900

    fix #118

--- kubernetes/mongodb/init/init.js
+++ kubernetes/mongodb/init/init.js
@@ -9,16 +9,17 @@ var users = [
       }
     ]
   },
-  {
-    user: "root",
-    pwd: "supersecret",
-    roles: [
-      { role: "clusterMonitor", db: "admin" },
-      { role: "read", db: "local" }
-    ]
-  }
 ];
 
 for (var i = 0, length = users.length; i < length; ++i) {
   db.createUser(users[i]);
 }
+
+db.getSiblingDB("admin").createUser({
+  user: "exporter",
+  pwd: "supersecret",
+  roles: [
+    { role: "clusterMonitor", db: "admin" },
+    { role: "read", db: "local" }
+  ]
+})
