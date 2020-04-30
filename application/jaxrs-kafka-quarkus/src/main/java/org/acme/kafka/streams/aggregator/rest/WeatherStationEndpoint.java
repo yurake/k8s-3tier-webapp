36fb0ccf7ee5032ca78bc16840d08013dd2f0a8a
YUSUKE URAKAWA <E35786@jp.ibm.com>
2020-04-30 19:59:39 +0900

    add server side events #14

--- application/jaxrs-kafka-quarkus/src/main/java/org/acme/kafka/streams/aggregator/rest/WeatherStationEndpoint.java
+++ application/jaxrs-kafka-quarkus/src/main/java/org/acme/kafka/streams/aggregator/rest/WeatherStationEndpoint.java
@@ -18,6 +18,9 @@ import javax.ws.rs.core.Response.Status;
 import org.acme.kafka.streams.aggregator.streams.GetWeatherStationDataResult;
 import org.acme.kafka.streams.aggregator.streams.InteractiveQueries;
 import org.acme.kafka.streams.aggregator.streams.PipelineMetadata;
+import org.apache.kafka.streams.KeyValue;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.annotations.SseElementType;
 
 @ApplicationScoped
 @Path("/quarkus/kafka")
@@ -25,6 +28,10 @@ public class WeatherStationEndpoint {
 
 	@Inject
 	InteractiveQueries interactiveQueries;
+
+	@Inject
+	@Channel("message")
+	KeyValue<Integer, String> message;
 
 	@GET
 	@Path("/data/{id}")
@@ -46,12 +53,20 @@ public class WeatherStationEndpoint {
 	}
 
 	@GET
-	@Path("/stream")
+	@Path("/stream/json")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getStreamData() {
 		List<String> result = interactiveQueries.getStreamData();
 		return Response.ok(result).build();
+	}
+
+	@GET
+	@Path("/stream/sse")
+	@Produces(MediaType.SERVER_SENT_EVENTS) // denotes that server side events (SSE) will be produced
+	@SseElementType("text/plain") // denotes that the contained data, within this SSE, is just regular text/plain data
+	public KeyValue<Integer, String> stream() {
+		return message;
 	}
 
 	@GET
