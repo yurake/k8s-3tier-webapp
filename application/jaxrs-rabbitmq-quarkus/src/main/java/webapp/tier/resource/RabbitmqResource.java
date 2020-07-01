8f4695101c53c9329b438e0e470fe3028189d722
yurak <devnulldevnull21@gmail.com>
2020-07-02 01:19:39 +0900

    change to testable code #158

--- application/jaxrs-rabbitmq-quarkus/src/main/java/webapp/tier/resource/RabbitmqResource.java
+++ application/jaxrs-rabbitmq-quarkus/src/main/java/webapp/tier/resource/RabbitmqResource.java
@@ -21,14 +21,13 @@ import webapp.tier.service.RabbitmqService;
 public class RabbitmqResource {
 
 	@Inject
-	RabbitmqService rabbitconn;
+	RabbitmqService svc;
 
 	@POST
 	@Path("/put")
 	@Counted(name = "performedChecks_put", description = "How many primality checks have been performed.")
 	@Timed(name = "checksTimer_put", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-	public Response set() {
-		RabbitmqService svc = new RabbitmqService();
+	public Response put() {
 		try {
 			return Response.ok().entity(svc.putMsg(svc.getConnection())).build();
 		} catch (Exception e) {
@@ -41,7 +40,6 @@ public class RabbitmqResource {
 	@Counted(name = "performedChecks_get", description = "How many primality checks have been performed.")
 	@Timed(name = "checksTimer_get", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
 	public Response get() {
-		RabbitmqService svc = new RabbitmqService();
 		try {
 			return Response.ok().entity(svc.getMsg(svc.getConnection())).build();
 		} catch (Exception e) {
@@ -54,7 +52,6 @@ public class RabbitmqResource {
 	@Counted(name = "performedChecks_publish", description = "How many primality checks have been performed.")
 	@Timed(name = "checksTimer_publish", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
 	public Response publish() {
-		RabbitmqService svc = new RabbitmqService();
 		try {
 			return Response.ok().entity(svc.publishMsg(svc.getConnection())).build();
 		} catch (Exception e) {
