d2bdb336544e175c443349b25ddfc9d78e7c5996
yurak <devnulldevnull21@gmail.com>
2020-07-13 23:44:13 +0900

    add resouce test cases #121

--- application/jaxrs-postgres-quarkus/src/test/java/webapp/tier/resource/PostgresResourceTest.java
+++ application/jaxrs-postgres-quarkus/src/test/java/webapp/tier/resource/PostgresResourceTest.java
@@ -1,8 +1,15 @@
 package webapp.tier.resource;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -10,36 +17,60 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 class PostgresResourceTest {
 
+	@BeforeEach
+	public void createTable() {
+		String createsql = "CREATE TABLE msg (id SERIAL PRIMARY KEY, msg TEXT NOT NULL)";
+		try (Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/mem:webapp;DB_CLOSE_DELAY=-1");
+				Statement stmt = con.createStatement()) {
+			stmt.executeUpdate(createsql);
+		} catch (SQLException e) {
+			e.fillInStackTrace();
+		}
+		return;
+	}
+
+	@AfterEach
+	private void dropTable() {
+		String createsql = "DROP TABLE msg";
+		try (Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/mem:webapp;DB_CLOSE_DELAY=-1");
+				Statement stmt = con.createStatement()) {
+			stmt.executeUpdate(createsql);
+		} catch (SQLException e) {
+			e.fillInStackTrace();
+		}
+		return;
+	}
+
 	@Test
-	void testInsertError() {
+	void testInsert() {
 		given()
 				.when()
 				.contentType("application/json")
 				.post("/quarkus/postgres/insert")
 				.then()
-				.statusCode(500)
-				.body(is("Insert Error."));
+				.statusCode(200)
+                .body(containsString("Hello k8s-3tier-webapp with quarkus"));
 	}
 
 	@Test
-	void testSelectError() {
+	void testSelect() {
 		given()
 				.when()
 				.get("/quarkus/postgres/select")
 				.then()
-				.statusCode(500)
-				.body(is("Select Error."));
+				.statusCode(200)
+                .body(containsString("No Data."));
 	}
 
 	@Test
-	void testDeleteError() {
+	void testDelete() {
 		given()
 				.when()
 				.contentType("application/json")
 				.post("/quarkus/postgres/delete")
 				.then()
-				.statusCode(500)
-				.body(is("Delete Error."));
+				.statusCode(200)
+                .body(containsString("Delete Msg Records"));
 	}
 
 }
