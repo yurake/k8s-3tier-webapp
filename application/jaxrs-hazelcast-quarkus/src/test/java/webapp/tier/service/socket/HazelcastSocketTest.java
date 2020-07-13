587814c49641f07070bb356a11e891a359af104b
yurak <devnulldevnull21@gmail.com>
2020-07-13 10:24:35 +0900

    add testcase for jaxrs-hazelcast socket #158

---
+++ application/jaxrs-hazelcast-quarkus/src/test/java/webapp/tier/service/socket/HazelcastSocketTest.java
@@ -0,0 +1,74 @@
+package webapp.tier.service.socket;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.inject.Inject;
+import javax.websocket.Session;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class HazelcastSocketTest {
+
+	@Inject
+	HazelcastSocket socket;
+
+	String id = "testOnOpen";
+	String message = "testMessage";
+
+	@Test
+	void testOnOpen() {
+		Session session = Mockito.mock(Session.class);
+		Mockito.when(session.getId()).thenReturn(id);
+		socket.onOpen(session);
+		assertThat(socket.getSessions().containsKey(id), is(true));
+	}
+
+	@Test
+	void testOnClose() {
+		Session session = Mockito.mock(Session.class);
+		Mockito.when(session.getId()).thenReturn(id);
+		socket.onOpen(session);
+		socket.onClose(session);
+		assertThat(socket.getSessions().containsKey(id), is(false));
+	}
+
+	@Test
+	void testOnError() {
+		Session session = Mockito.mock(Session.class);
+		Mockito.when(session.getId()).thenReturn(id);
+		socket.onOpen(session);
+		socket.onError(session, new Throwable("testOnError"));
+		assertThat(socket.getSessions().containsKey(id), is(false));
+	}
+
+	@Test
+	void testOnMessage() {
+		Session session = Mockito.mock(Session.class);
+		Mockito.when(session.getId()).thenReturn(id);
+		socket.onOpen(session);
+		try {
+			socket.onMessage(message);
+			fail();
+		} catch (Exception expected) {
+			expected.printStackTrace();
+		} finally {
+			socket.onClose(session);
+		}
+	}
+
+	@Test
+	void testOnMessageNoSubscriber() {
+		try {
+			socket.onMessage(message);
+		} catch (Exception expected) {
+			expected.printStackTrace();
+			fail();
+		}
+	}
+}
