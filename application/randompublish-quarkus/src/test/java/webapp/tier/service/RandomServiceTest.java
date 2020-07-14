d75883e04b9a502b202b388112391a5c1d798516
yurak <devnulldevnull21@gmail.com>
2020-07-15 08:46:51 +0900

    add test count for random case

--- application/randompublish-quarkus/src/test/java/webapp/tier/service/RandomServiceTest.java
+++ application/randompublish-quarkus/src/test/java/webapp/tier/service/RandomServiceTest.java
@@ -1,5 +1,7 @@
 package webapp.tier.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class RandomServiceTest {
@@ -7,12 +9,12 @@ class RandomServiceTest {
 	@Test
 	void testDeliverrandom() {
 		RandomService srv = new RandomService();
-		int counts = 10;
+		int counts = 50;
 		for (int i = 0; i < counts; i++) {
 			try {
 				srv.deliverrandom();
+				fail();
 			} catch (Exception e) {
-				e.printStackTrace();
 			}
 		}
 	}
