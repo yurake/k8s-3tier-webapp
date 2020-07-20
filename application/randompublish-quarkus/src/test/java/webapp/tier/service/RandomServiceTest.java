ff76b0e487b04ceb8c93b76190abc8eaeed24ae8
yurak <devnulldevnull21@gmail.com>
2020-07-20 22:36:13 +0900

    Inject random int method in random service #303

--- application/randompublish-quarkus/src/test/java/webapp/tier/service/RandomServiceTest.java
+++ application/randompublish-quarkus/src/test/java/webapp/tier/service/RandomServiceTest.java
@@ -1,12 +1,98 @@
 package webapp.tier.service;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
 class RandomServiceTest {
 
 	@Test
+	void testDeliverrandomCase0Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 0;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testDeliverrandomCase1Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 1;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testDeliverrandomCase2Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 2;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testDeliverrandomCase3Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 3;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testDeliverrandomCase4Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 4;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testDeliverrandomCase5Error() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 5;
 	void testDeliverrandom() {
 		RandomService srv = new RandomService();
 		int counts = 50;
@@ -16,7 +102,35 @@ class RandomServiceTest {
 				fail();
 			} catch (Exception e) {
 			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
+	@Test
+	void testDeliverrandomError() {
+		RandomService srv = new RandomService() {
+			protected int getNum(Integer i) {
+				return 9999;
+			}
+		};
+		try {
+			srv.deliverrandom();
+			fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testGetNum() {
+		RandomService svc = new RandomService();
+		Integer i = svc.getNum(6);
+		assertThat(i, greaterThanOrEqualTo(0));
+		assertThat(i, lessThanOrEqualTo(5));
+	}
 }
