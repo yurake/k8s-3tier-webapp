d5b5afd99b7122eeb6fa3f77f768646b1b9c5a09
yurak <devnulldevnull21@gmail.com>
2020-08-25 01:36:34 +0900

    improve coverage #392

--- application/consumer-hazelcast-quarkus/src/main/java/webapp/tier/service/HazelcastDeliverSubscriber.java
+++ application/consumer-hazelcast-quarkus/src/main/java/webapp/tier/service/HazelcastDeliverSubscriber.java
@@ -9,23 +9,24 @@ import javax.enterprise.inject.spi.CDI;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import com.hazelcast.topic.Message;
+
 import webapp.tier.bean.MsgBean;
 import webapp.tier.util.MsgUtils;
 
 @ApplicationScoped
-public class RedisDeliverSubscriber extends RedisSubscriber {
+public class HazelcastDeliverSubscriber extends HazelcastMessageListener {
 
-	private static final Logger LOG = Logger.getLogger(RedisSubscriber.class.getSimpleName());
-	private static String splitkey = ConfigProvider.getConfig().getValue("redis.splitkey", String.class);
+	private static final Logger LOG = Logger.getLogger(HazelcastDeliverSubscriber.class.getSimpleName());
+	private static String splitkey = ConfigProvider.getConfig().getValue("hazelcast.split.key", String.class);
 
 	@Override
-	public void onMessage(String channel, String message) {
-		MsgBean msgbean = MsgUtils.splitBody(message, splitkey);
+	public void onMessage(Message<Object> message) {
+		MsgBean msgbean = MsgUtils.splitBody(message.getMessageObject().toString(), splitkey);
 		msgbean.setFullmsg("Received");
 		LOG.log(Level.INFO, msgbean.getFullmsg());
 		DeliverService deliversvc = CDI.current().select(DeliverService.class, RestClient.LITERAL).get();
 		String response = deliversvc.random();
 		LOG.log(Level.INFO, "Call Random Publish: {0}", response);
 	}
-
 }
--- application/consumer-hazelcast-quarkus/src/test/java/webapp/tier/service/HazelcastSubscribeServiceTest.java
+++ application/consumer-hazelcast-quarkus/src/test/java/webapp/tier/service/HazelcastSubscribeServiceTest.java
@@ -1,10 +1,19 @@
 package webapp.tier.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
 
 import javax.inject.Inject;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -14,18 +23,32 @@ class HazelcastSubscribeServiceTest {
 	@Inject
 	HazelcastSubscribeService svc;
 
+	private static HazelcastInstance mockInstance;
+	String respbody = "message: Hello k8s-3tier-webapp with quarkus";
+
+	@BeforeEach
+	public void setup() throws IOException {
+		mockInstance = Hazelcast.newHazelcastInstance();
+	}
+
+	@AfterEach
+	public void after() {
+		mockInstance.shutdown();
+	}
+
 	@Test
-	void testOnStartError() {
+	void testSubscribeHazelcast() {
+		svc.subscribeHazelcast(mockInstance, svc.createHazelcastDeliverSubscriber());
+		assertThat(svc.publishMsg(mockInstance).getFullmsg(), containsString(respbody));
+	}
+
+	@Test
+	void testSubscribeNoData() {
 		try {
 			svc.run();
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail();
 		}
 	}
-
-	@Test
-	void testIsActiveError() {
-		assertEquals(false, svc.isActive(), "Unexpected status");
-	}
-
 }
