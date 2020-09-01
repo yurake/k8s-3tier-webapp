672d011479ce0f3fdfd28d126a63c362c5d9c402
yurak <devnulldevnull21@gmail.com>
2020-09-02 00:55:08 +0900

    Update MessageGeneratorTest.java

--- application/producer-kafka-quarkus/src/test/java/webapp/tier/service/generator/MessageGeneratorTest.java
+++ application/producer-kafka-quarkus/src/test/java/webapp/tier/service/generator/MessageGeneratorTest.java
@@ -2,6 +2,8 @@ package webapp.tier.service.generator;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,9 +11,11 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 class MessageGeneratorTest {
 
+	@Inject
+	private MessageGenerator gen;
+
 	@Test
-	void test() {
-		MessageGenerator gen = new MessageGenerator();
+	void testGenerate() {
 		try {
 			gen.generate();
 		} catch (Exception e) {
