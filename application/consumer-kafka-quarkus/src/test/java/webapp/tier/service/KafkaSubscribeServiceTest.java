746a75b3533d2b9234006cf15493068c614d2306
yurak <devnulldevnull21@gmail.com>
2020-09-01 22:54:59 +0900

    update coverage for consumer-kafka-quarkus #392

--- application/consumer-kafka-quarkus/src/test/java/webapp/tier/service/KafkaSubscribeServiceTest.java
+++ application/consumer-kafka-quarkus/src/test/java/webapp/tier/service/KafkaSubscribeServiceTest.java
@@ -2,22 +2,39 @@ package webapp.tier.service;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.reactive.messaging.annotations.Merge;
 
 @QuarkusTest
 class KafkaSubscribeServiceTest {
 
+	@Inject
+	KafkaSubscribeService svc;
+
+	@Inject
+	@Channel("message")
+	@Merge(Merge.Mode.MERGE)
+	Emitter<String> emitmsg;
+
 	@Test
-	void test() {
-		KafkaSubscribeService svc = new KafkaSubscribeService();
+	void testProcess() {
 		try {
-		svc.process("Test");
-		fail();
+			svc.process("Test");
 		} catch (Exception e) {
 			e.printStackTrace();
+			fail();
 		}
+	}
+
+	@Test
+	void testProcessEmitter() {
+		emitmsg.send("Test");
 	}
 
 }
