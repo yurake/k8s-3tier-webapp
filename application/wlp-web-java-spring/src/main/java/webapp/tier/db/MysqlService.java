c592d79926bfcf6819a0492c4b6248aad4579b34
yurak <devnulldevnull21@gmail.com>
2020-09-23 00:25:55 +0900

    refactor mysql service for wlp #392

--- application/wlp-web-java-spring/src/main/java/webapp/tier/db/MysqlService.java
+++ application/wlp-web-java-spring/src/main/java/webapp/tier/db/MysqlService.java
@@ -21,14 +21,14 @@ import webapp.tier.util.GetConfig;
 public class MysqlService {
 
 	private static Logger logger = LoggerFactory.getLogger(MysqlService.class);
+	private static String jndiname = GetConfig.getResourceBundle("mysql.jndi.name");
 	private static String sqlkey = GetConfig.getResourceBundle("mysql.insert.msg.id");
 	private static String sqlbody = GetConfig.getResourceBundle("mysql.insert.msg.body");
 	private static String message = GetConfig.getResourceBundle("common.message");
-	private static String jndiname = GetConfig.getResourceBundle("mysql.jndi.name");
-	private static String sqlselect = GetConfig.getResourceBundle("mysql.select.msg.all");
-	private static String sqldelete = GetConfig.getResourceBundle("mysql.delete.msg.all");
-	private DataSource ds;
-	private Connection con = null;
+	private static String insertsql = GetConfig.getResourceBundle("mysql.insert.msg");
+	private static String selectsql = GetConfig.getResourceBundle("mysql.select.msg.all");
+	private static String deletesql = GetConfig.getResourceBundle("mysql.delete.msg.all");
+	private DataSource ds = null;
 
 	public Connection getConnection() throws SQLException, NamingException {
 		InitialContext ctx = new InitialContext();
@@ -38,87 +38,51 @@ public class MysqlService {
 
 	public boolean connectionStatus() {
 		boolean status = false;
-		try {
-			con = getConnection();
+		try (Connection con = getConnection()) {
 			status = true;
 		} catch (SQLException | NamingException e) {
 			e.printStackTrace();
-		} finally {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 		return status;
 	}
 
 	public String insert() throws SQLException, NamingException, NoSuchAlgorithmException {
-		Connection con = null;
+		String sql = null;
 		String id = String.valueOf(CreateId.createid());
-		String sql = GetConfig.getResourceBundle("mysql.insert.msg");
 
-		sql = sql.replace(sqlkey, id);
-		sql = sql.replace(sqlbody, message);
-
-		MysqlService conmysql = new MysqlService();
-		con = conmysql.getConnection();
-		Statement stmt = con.createStatement();
-
-		logger.info("Execute SQL: " + sql);
-		stmt.executeUpdate(sql);
-
-		if (con != null) {
-			con.close();
+		try (Connection con = getConnection();
+				Statement stmt = con.createStatement()) {
+			sql = insertsql.replace(sqlkey, id).replace(sqlbody, message);
+			logger.info("Execute SQL: " + sql);
+			stmt.executeUpdate(sql);
 		}
 		return sql;
 	}
 
 	public List<String> select() throws SQLException, NamingException {
 		List<String> allmsg = new ArrayList<>();
-		MysqlService conmysql = new MysqlService();
-		Connection con = conmysql.getConnection();
-		Statement stmt = con.createStatement();
 
-		logger.info("Execute SQL: " + sqlselect);
-		ResultSet rs = stmt.executeQuery(sqlselect);
-
-		while (rs.next()) {
-			String fullmsg = "Selected Msg: id: " + rs.getString("id") + ", message: " + rs.getString("msg");
-			logger.info(fullmsg);
-			allmsg.add(fullmsg);
-		}
-		if (allmsg.isEmpty()) {
-			allmsg.add("No Data");
-		}
-
-		if (con != null) {
-			con.close();
+		try (Connection con = getConnection();
+				Statement stmt = con.createStatement()) {
+			logger.info("Execute SQL: " + selectsql);
+			ResultSet rs = stmt.executeQuery(selectsql);
+			while (rs.next()) {
+				String fullmsg = "Selected Msg: id: " + rs.getString("id") + ", message: " + rs.getString("msg");
+				logger.info(fullmsg);
+				allmsg.add(fullmsg);
+			}
+			if (allmsg.isEmpty()) {
+				allmsg.add("No Data");
+			}
 		}
 		return allmsg;
 	}
 
 	public String delete() throws SQLException, NamingException {
-		Connection con = null;
-
-		try {
-			MysqlService conmysql = new MysqlService();
-			con = conmysql.getConnection();
-			Statement stmt = con.createStatement();
-
-			logger.info("Delete SQL: " + sqldelete);
-			stmt.executeUpdate(sqldelete);
-
-		} finally {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
+		try (Connection con = getConnection();
+				Statement stmt = con.createStatement()) {
+			logger.info("Delete SQL: " + deletesql);
+			stmt.executeUpdate(deletesql);
 		}
 		return "Deleted";
 	}
