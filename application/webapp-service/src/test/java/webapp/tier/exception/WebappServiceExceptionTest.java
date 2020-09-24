f07644ce405051539b9d978c3d9a0fd5e9c7e835
yurak <devnulldevnull21@gmail.com>
2020-09-25 00:39:49 +0900

    add test case #537

---
+++ application/webapp-service/src/test/java/webapp/tier/exception/WebappServiceExceptionTest.java
@@ -0,0 +1,21 @@
+package webapp.tier.exception;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class WebappServiceExceptionTest {
+
+	@Test
+	void testWebappServiceException() {
+		RuntimeException ex = new RuntimeException();
+		WebappServiceException e = new WebappServiceException("Error", ex);
+		assertThat(e.getClass(), is(WebappServiceException.class));
+		assertThat(e.getMessage(), is("Error"));
+	}
+
+}
