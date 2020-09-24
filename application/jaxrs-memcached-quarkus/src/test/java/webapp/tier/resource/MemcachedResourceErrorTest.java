19048304aaa2f36ee260579704c59cade774a681
yurak <devnulldevnull21@gmail.com>
2020-09-24 21:37:35 +0900

    improve coverage jaxrs-memcached-quarkus #392

--- application/jaxrs-memcached-quarkus/src/test/java/webapp/tier/resource/MemcachedResourceErrorTest.java
+++ application/jaxrs-memcached-quarkus/src/test/java/webapp/tier/resource/MemcachedResourceErrorTest.java
@@ -1,35 +1,48 @@
 package webapp.tier.resource;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.CoreMatchers.*;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.security.NoSuchAlgorithmException;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import webapp.tier.service.MemcachedService;
 
 @QuarkusTest
 class MemcachedResourceErrorTest {
 
+	@InjectMock
+	MemcachedService svc;
 
 	@Test
 	void testSetError() {
+		try {
+			when(svc.setMsg(ArgumentMatchers.any())).thenThrow(new RuntimeException());
+		} catch (NoSuchAlgorithmException | RuntimeException e) {
+			e.printStackTrace();
+			fail();
+		}
 		given()
 				.when()
 				.contentType("application/json")
 				.post("/quarkus/memcached/set")
 				.then()
-				.statusCode(500)
-				.body(is("Set Error."));
+				.statusCode(500);
 	}
 
 	@Test
-	void testGetNoData() {
+	void testGetError() {
+		when(svc.getMsg(ArgumentMatchers.any())).thenThrow(new RuntimeException());
 		given()
 				.when()
 				.get("/quarkus/memcached/get")
 				.then()
-				.statusCode(200)
-				.body("message", equalTo("No Data."));
+				.statusCode(500);
 	}
 
 }
