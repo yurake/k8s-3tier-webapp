3c3e8303279e4894c8bc064d0f91cda2a4f70bb3
Restyled.io <commits@restyled.io>
2020-09-29 16:00:41 +0000

    Restyled by prettier

--- kubernetes/nginx/docs/js/jaxrs.js
+++ kubernetes/nginx/docs/js/jaxrs.js
@@ -1,23 +1,23 @@
 root = location.href;
-url = root + 'quarkus';
-kafkaurl = url + '/kafka/';
-memcachedurl = url + '/memcached/';
-redisurl = url + '/redis/';
-rabbitmqurl = url + '/rabbitmq/';
-activemqurl = url + '/activemq/';
-hazelcasturl = url + '/hazelcast/';
-mysqlurl = url + '/mysql/';
-postgresurl = url + '/postgres/';
-mongodburl = url + '/mongodb/';
+url = root + "quarkus";
+kafkaurl = url + "/kafka/";
+memcachedurl = url + "/memcached/";
+redisurl = url + "/redis/";
+rabbitmqurl = url + "/rabbitmq/";
+activemqurl = url + "/activemq/";
+hazelcasturl = url + "/hazelcast/";
+mysqlurl = url + "/mysql/";
+postgresurl = url + "/postgres/";
+mongodburl = url + "/mongodb/";
 initialvalue = "Response Values";
-set = 'set';
-put = 'put';
-get = 'get';
-post = 'post';
-publish = 'publish';
-insert = 'insert';
-select = 'select';
-delt = 'delete';
+set = "set";
+put = "put";
+get = "get";
+post = "post";
+publish = "publish";
+insert = "insert";
+select = "select";
+delt = "delete";
 respkafka = "#respkafka";
 respmemcached = "#respmemcached";
 respredis = "#respredis";
@@ -34,7 +34,7 @@ isopenrabbitmq = false;
 isopenactivemq = false;
 isopenhazelcast = false;
 
-$(function() {
+$(function () {
   $(respkafka).html(initialvalue);
   $(respmemcached).html(initialvalue);
   $(respredis).html(initialvalue);
@@ -50,40 +50,40 @@ $(function() {
 
   function dispMsgFromString(type, url, respid) {
     $.ajax({
-       type : type,
-       url : url,
-       cache : false,
-       timeout : 10000,
-     }).always(function(data) {
+      type: type,
+      url: url,
+      cache: false,
+      timeout: 10000,
+    }).always(function (data) {
       const resp = JSON.parse(JSON.stringify(data, null, 4));
       console.log(resp);
-      $(respid).html(resp.responseText + '\nstatus: ' + resp.status);
+      $(respid).html(resp.responseText + "\nstatus: " + resp.status);
     });
   }
 
   function dispMsgFromJson(type, url, respid) {
     $.ajax({
-       type : type,
-       url : url,
-       cache : false,
-       timeout : 10000,
-     }).always(function(data, jqXHR) {
+      type: type,
+      url: url,
+      cache: false,
+      timeout: 10000,
+    }).always(function (data, jqXHR) {
       console.log(data);
-      $(respid).html(data.fullmsg + '\nstatus: ' + jqXHR.status);
+      $(respid).html(data.fullmsg + "\nstatus: " + jqXHR.status);
     });
   }
 
   function dispMsgFromJsonArray(type, url, respid) {
     $.ajax({
-       type : type,
-       url : url,
-       cache : false,
-       timeout : 10000,
-     }).always(function(data, jqXHR) {
+      type: type,
+      url: url,
+      cache: false,
+      timeout: 10000,
+    }).always(function (data, jqXHR) {
       var resparray = "";
       for (i = 0; i < data.length; i++) {
         if (data[i] != null) {
-          resparray += data[i].fullmsg + '\n';
+          resparray += data[i].fullmsg + "\n";
         }
       }
       $(respid).html(resparray + jqXHR.status);
@@ -97,11 +97,13 @@ $(function() {
     if (isopen) {
       console.log("Already connected.");
     } else {
-      ws = new WebSocket('ws://k8s.3tier.webapp/quarkus/' + svc + '/subscribe');
+      ws = new WebSocket("ws://k8s.3tier.webapp/quarkus/" + svc + "/subscribe");
       isopen = true;
       console.log("Connection to server opened.");
-      ws.onopen = function() {
-        ws.onmessage = function(receive) { $(resp).text(receive.data); };
+      ws.onopen = function () {
+        ws.onmessage = function (receive) {
+          $(resp).text(receive.data);
+        };
       };
     }
   }
@@ -120,21 +122,22 @@ $(function() {
     }
   }
 
-  $("#getkafka")
-      .click(function() { dispMsgFromString(get, kafkaurl + get, respkafka); });
+  $("#getkafka").click(function () {
+    dispMsgFromString(get, kafkaurl + get, respkafka);
+  });
 
-  $("#publishkafka").click(function() {
+  $("#publishkafka").click(function () {
     dispMsgFromJson(post, kafkaurl + publish, respkafka);
   });
 
-  $("#subscribekafka").click(function() {
+  $("#subscribekafka").click(function () {
     if (isopenkafka) {
       console.log("Already connected.");
     } else {
       console.log("Connection to server opened.");
       isopenkafka = true;
-      sse = new EventSource(kafkaurl + 'subscribe');
-      sse.onmessage = function(event) {
+      sse = new EventSource(kafkaurl + "subscribe");
+      sse.onmessage = function (event) {
         const resp = event.data;
         console.log(resp);
         document.getElementById("respkafka").innerHTML = resp;
@@ -142,7 +145,7 @@ $(function() {
     }
   });
 
-  $("#stopkafka").click(function() {
+  $("#stopkafka").click(function () {
     if (isopenkafka) {
       console.log("Connection to server closed.");
       sse.close();
@@ -151,119 +154,135 @@ $(function() {
     }
   });
 
-  $("#setmemcached").click(function() {
+  $("#setmemcached").click(function () {
     dispMsgFromJson(post, memcachedurl + set, respmemcached);
   });
 
-  $("#getmemcached").click(function() {
+  $("#getmemcached").click(function () {
     dispMsgFromJson(get, memcachedurl + get, respmemcached);
   });
 
-  $("#putredis")
-      .click(function() { dispMsgFromJson(post, redisurl + put, respredis); });
+  $("#putredis").click(function () {
+    dispMsgFromJson(post, redisurl + put, respredis);
+  });
 
-  $("#getredis").click(function() {
+  $("#getredis").click(function () {
     dispMsgFromJsonArray(get, redisurl + get, respredis);
   });
 
-  $("#subscriberedis").click(function() { subscribeWebSocket("redis"); });
+  $("#subscriberedis").click(function () {
+    subscribeWebSocket("redis");
+  });
 
-  $("#stopredis").click(function() { stopSweSocket("redis"); });
+  $("#stopredis").click(function () {
+    stopSweSocket("redis");
+  });
 
-  $("#publishredis").click(function() {
+  $("#publishredis").click(function () {
     dispMsgFromJson(post, redisurl + publish, respredis);
   });
 
-  $("#putrabbitmq").click(function() {
+  $("#putrabbitmq").click(function () {
     dispMsgFromJson(post, rabbitmqurl + put, resprabbitmq);
   });
 
-  $("#getrabbitmq").click(function() {
+  $("#getrabbitmq").click(function () {
     dispMsgFromJson(get, rabbitmqurl + get, resprabbitmq);
   });
 
-  $("#publishrabbitmq").click(function() {
+  $("#publishrabbitmq").click(function () {
     dispMsgFromJson(post, rabbitmqurl + publish, resprabbitmq);
   });
 
-  $("#subscriberabbitmq").click(function() { subscribeWebSocket("rabbitmq"); });
+  $("#subscriberabbitmq").click(function () {
+    subscribeWebSocket("rabbitmq");
+  });
 
-  $("#stoprabbitmq").click(function() { stopSweSocket("rabbitmq"); });
+  $("#stoprabbitmq").click(function () {
+    stopSweSocket("rabbitmq");
+  });
 
-  $("#putactivemq").click(function() {
+  $("#putactivemq").click(function () {
     dispMsgFromJson(post, activemqurl + put, respactivemq);
   });
 
-  $("#getactivemq").click(function() {
+  $("#getactivemq").click(function () {
     dispMsgFromJson(get, activemqurl + get, respactivemq);
   });
 
-  $("#publishactivemq").click(function() {
+  $("#publishactivemq").click(function () {
     dispMsgFromJson(post, activemqurl + publish, respactivemq);
   });
 
-  $("#subscribeactivemq").click(function() { subscribeWebSocket("activemq"); });
-
-  $("#stopactivemq").click(function() { stopSweSocket("activemq"); });
-
-  $("#setcachehazelcast").click(function() {
-    dispMsgFromJson(post, hazelcasturl + 'setcache', respcachehazelcast);
+  $("#subscribeactivemq").click(function () {
+    subscribeWebSocket("activemq");
   });
 
-  $("#getcachehazelcast").click(function() {
-    dispMsgFromJsonArray(get, hazelcasturl + 'getcache', respcachehazelcast);
+  $("#stopactivemq").click(function () {
+    stopSweSocket("activemq");
   });
 
-  $("#publishhazelcast").click(function() {
+  $("#setcachehazelcast").click(function () {
+    dispMsgFromJson(post, hazelcasturl + "setcache", respcachehazelcast);
+  });
+
+  $("#getcachehazelcast").click(function () {
+    dispMsgFromJsonArray(get, hazelcasturl + "getcache", respcachehazelcast);
+  });
+
+  $("#publishhazelcast").click(function () {
     dispMsgFromJson(post, hazelcasturl + publish, respcachehazelcast);
   });
 
-  $("#subscribehazelcast")
-      .click(function() { subscribeWebSocket("hazelcast"); });
-
-  $("#stophazelcast").click(function() { stopSweSocket("hazelcast"); });
-
-  $("#putqueuehazelcast").click(function() {
-    dispMsgFromJson(post, hazelcasturl + 'putqueue', respqueuehazelcast);
+  $("#subscribehazelcast").click(function () {
+    subscribeWebSocket("hazelcast");
   });
 
-  $("#getqueuehazelcast").click(function() {
-    dispMsgFromJson(get, hazelcasturl + 'getqueue', respqueuehazelcast);
+  $("#stophazelcast").click(function () {
+    stopSweSocket("hazelcast");
   });
 
-  $("#insertmysql").click(function() {
+  $("#putqueuehazelcast").click(function () {
+    dispMsgFromJson(post, hazelcasturl + "putqueue", respqueuehazelcast);
+  });
+
+  $("#getqueuehazelcast").click(function () {
+    dispMsgFromJson(get, hazelcasturl + "getqueue", respqueuehazelcast);
+  });
+
+  $("#insertmysql").click(function () {
     dispMsgFromJson(post, mysqlurl + insert, respmysql);
   });
 
-  $("#selectmysql").click(function() {
+  $("#selectmysql").click(function () {
     dispMsgFromJsonArray(get, mysqlurl + select, respmysql);
   });
 
-  $("#deletemysql").click(function() {
+  $("#deletemysql").click(function () {
     dispMsgFromString(post, mysqlurl + delt, respmysql);
   });
 
-  $("#insertpostgres").click(function() {
+  $("#insertpostgres").click(function () {
     dispMsgFromJson(post, postgresurl + insert, resppostgres);
   });
 
-  $("#selectpostgres").click(function() {
+  $("#selectpostgres").click(function () {
     dispMsgFromJsonArray(get, postgresurl + select, resppostgres);
   });
 
-  $("#deletepostgres").click(function() {
+  $("#deletepostgres").click(function () {
     dispMsgFromString(post, postgresurl + delt, resppostgres);
   });
 
-  $("#insertmongodb").click(function() {
+  $("#insertmongodb").click(function () {
     dispMsgFromJson(post, mongodburl + insert, respmongodb);
   });
 
-  $("#selectmongodb").click(function() {
+  $("#selectmongodb").click(function () {
     dispMsgFromJsonArray(get, mongodburl + select, respmongodb);
   });
 
-  $("#deletemongodb").click(function() {
+  $("#deletemongodb").click(function () {
     dispMsgFromString(post, mongodburl + delt, respmongodb);
   });
 });
