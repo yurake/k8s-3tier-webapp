f41cb480eb39287b55271d8b60b57ffdc175238a
yurak <devnulldevnull21@gmail.com>
2020-10-29 08:57:10 +0900

    move ignore shellcheck

--- kubernetes/cassandra/entrypoint-wrap.sh
+++ kubernetes/cassandra/entrypoint-wrap.sh
@@ -6,12 +6,12 @@ set -e
 
 # first arg is `-f` or `--some-option`
 # or there are no args
-# shellcheck disable=SC2166
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
   set -- cassandra -f "$@"
 fi
 
 # allow the container to be started with `--user`
+# shellcheck disable=SC2166
 if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
   find "$CASSANDRA_CONF" /var/lib/cassandra /var/log/cassandra \
     \! -user cassandra -exec chown cassandra '{}' +
