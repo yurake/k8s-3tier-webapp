d0ff2d61f0c7a718f1819df3115fd91522ea6dad
yurak <devnulldevnull21@gmail.com>
2021-01-23 23:03:48 +0900

    change xml to com.hazelcast.client.config.ClientConfig #1873

--- application/wlp-web-java-spring/src/main/java/webapp/tier/util/HazelcastInstanceConfigurator.java
+++ application/wlp-web-java-spring/src/main/java/webapp/tier/util/HazelcastInstanceConfigurator.java
@@ -4,18 +4,18 @@ import java.io.IOException;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
 
 public class HazelcastInstanceConfigurator {
 
-	private static String clientxml = GetConfig.getResourceBundle("hazelcast.client.xml");
+	private static String address = GetConfig.getResourceBundle("hazelcast.address");
 
 	private HazelcastInstanceConfigurator() {
 	}
 
 	public static HazelcastInstance getInstance() throws IOException {
-		ClientConfig clientConfig = new XmlClientConfigBuilder(clientxml).build();
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().addAddress(address);
 		clientConfig.getConnectionStrategyConfig().getConnectionRetryConfig()
 				.setClusterConnectTimeoutMillis(1000)
 				.setMaxBackoffMillis(2000);
