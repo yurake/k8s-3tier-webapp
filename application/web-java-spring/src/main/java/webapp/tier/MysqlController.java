9fa7e5b667a876c537945bec8db5123058aabd49
yurak <devnulldevnull21@gmail.com>
2021-02-10 00:33:01 +0900

    add logger #1970

--- application/web-java-spring/src/main/java/webapp/tier/MysqlController.java
+++ application/web-java-spring/src/main/java/webapp/tier/MysqlController.java
@@ -1,11 +1,15 @@
 package webapp.tier;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +29,7 @@ import webapp.tier.util.GetConfig;
 @Consumes(MediaType.APPLICATION_JSON)
 public class MysqlController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 	private static String message = GetConfig.getResourceBundle("common.message");
 
 	@Autowired
@@ -41,18 +46,37 @@ public class MysqlController {
 		Message m = new Message();
 		m.setId(id);
 		m.setMsg(message);
+		logger.info(logMessageOut("insert", m.getId(), m.getMsg()));
 
 		return messageRepository.save(m);
 	}
 
 	@GetMapping("/select")
 	public @ResponseBody Iterable<Message> select() {
-		return messageRepository.findAll();
+		List<Message> allmsg = new ArrayList<>();
+		allmsg = messageRepository.findAll();
+
+		if (allmsg.isEmpty()) {
+			Message m = new Message();
+			m.setId(0);
+			m.setMsg("No Data");
+			allmsg.add(m);
+		}
+
+		for(Message value : allmsg) {
+			logger.info(logMessageOut("select", value.getId(), value.getMsg()));
+		}
+
+		return allmsg;
 	}
 
 	@PostMapping("/delete")
 	public @ResponseBody String delete() {
 		messageRepository.deleteAll();
 		return "Deleted";
+	}
+
+	private String logMessageOut(String type, int id, String msg) {
+		return type + ": id: " + id + ", msg: " + msg;
 	}
 }
