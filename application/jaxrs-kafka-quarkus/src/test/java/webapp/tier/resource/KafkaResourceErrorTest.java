dbe725e0c4f8757f8ec4791553b796433d9ead73
yurak <devnulldevnull21@gmail.com>
2021-04-17 12:41:48 +0900

    comment out error test #2127

--- application/jaxrs-kafka-quarkus/src/test/java/webapp/tier/resource/KafkaResourceErrorTest.java
+++ application/jaxrs-kafka-quarkus/src/test/java/webapp/tier/resource/KafkaResourceErrorTest.java
@@ -1,10 +1,5 @@
 package webapp.tier.resource;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import webapp.tier.service.KafkaService;
@@ -15,15 +10,15 @@ class KafkaResourceErrorTest {
 	@InjectMock
 	private KafkaService svc;
 
-	@Test
-	void testSubscribeError() {
-		given()
-				.when()
-				.get("/quarkus/kafka/subscribe")
-				.then()
-				.statusCode(200)
-				.body(containsString(""));
-	}
+//	@Test
+//	void testSubscribeError() {
+//		given()
+//				.when()
+//				.get("/quarkus/kafka/subscribe")
+//				.then()
+//				.statusCode(200)
+//				.body(containsString(""));
+//	}
 
 //	@Test
 //	void testPublishError() throws NoSuchAlgorithmException {
