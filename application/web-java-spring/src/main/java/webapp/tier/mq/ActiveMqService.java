5b758d06102998a49472fd0c5e0ab4835ec4d5ae
yurak <devnulldevnull21@gmail.com>
2021-04-20 00:47:44 +0900

    delete not used configuration #2070

--- application/web-java-spring/src/main/java/webapp/tier/mq/ActiveMqService.java
+++ application/web-java-spring/src/main/java/webapp/tier/mq/ActiveMqService.java
@@ -3,16 +3,10 @@ package webapp.tier.mq;
 import java.util.Objects;
 
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-import javax.jms.Topic;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,22 +16,15 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import webapp.tier.bean.MsgBean;
-import webapp.tier.util.CreateId;
 
 @Service
 public class ActiveMqService {
-
-	@Autowired
-	private Queue queue;
 
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
 	@Value("${common.message}")
 	private String message;
-
-	@Value("${spring.activemq.broker-url}")
-	private String url;
 
 	@Value("${spring.activemq.split.key}")
 	private String splitkey;
@@ -82,92 +69,4 @@ public class ActiveMqService {
 		logger.info(msgbean.logMessageOut("publish"));
 		return msgbean;
 	}
-
-	private InitialContext getInitialContext() throws NamingException {
-		return new InitialContext();
-	}
-
-	public Session getConnection() throws Exception {
-		ConnectionFactory cf = (ConnectionFactory) getInitialContext().lookup("jms/QueueConnectionFactory");
-		conn = cf.createConnection();
-		conn.start();
-		session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		return session;
-	}
-
-	public Queue getQueue() throws Exception {
-		return (Queue) getInitialContext().lookup("jms/ActiveMQueue");
-	}
-
-	public Topic getTopic() throws Exception {
-		return (Topic) getInitialContext().lookup("jms/ActiveMQTopic");
-	}
-
-	public void close() throws JMSException {
-		if (session != null) {
-			session.close();
-		}
-		if (conn != null) {
-			conn.close();
-		}
-	}
-
-	public String putActiveMq() throws Exception {
-		String fullmsg = null;
-		String id = String.valueOf(CreateId.createid());
-
-		try {
-			StringBuilder buf = new StringBuilder();
-			String body = buf.append(id).append(splitkey).append(message).toString();
-			getConnection();
-			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			TextMessage message = session.createTextMessage(body);
-			Queue q = getQueue();
-			session.createProducer(q).send(message);
-			fullmsg = "Set id: " + id + ", msg: " + message;
-			logger.info(fullmsg);
-		} finally {
-			close();
-		}
-		return fullmsg;
-	}
-
-	public String getActiveMq() throws Exception {
-		String fullmsg = "No Data";
-
-		try {
-			getConnection();
-			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			MessageConsumer consumer = session.createConsumer(getQueue());
-			TextMessage message = (TextMessage) consumer.receive(1000);
-
-			if (Objects.nonNull(message)) {
-				String[] body = message.getText().split(splitkey, 0);
-				fullmsg = "Received id: " + body[0] + ", msg: " + body[1];
-			}
-			logger.info(fullmsg);
-		} finally {
-			close();
-		}
-		return fullmsg;
-	}
-
-	public String publishActiveMq() throws Exception {
-		String fullmsg = "Error";
-		String id = String.valueOf(CreateId.createid());
-
-		try {
-			StringBuilder buf = new StringBuilder();
-			String body = buf.append(id).append(splitkey).append(message).toString();
-			getConnection();
-			TextMessage message = session.createTextMessage(body);
-			session.createProducer(getTopic()).send(message);
-			fullmsg = "Publish id: " + id + ", msg: " + message;
-			logger.info(fullmsg);
-		} finally {
-			close();
-		}
-		return fullmsg;
-	}
-
 }
