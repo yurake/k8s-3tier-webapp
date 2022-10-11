a98a09c4331855bd70dc300182c4ab55d7078062
yurak <devnulldevnull21@gmail.com>
2022-10-11 23:18:49 +0900

    migrate to use quarkus-redis-client #2739

--- application/jaxrs-redis-quarkus/src/test/java/webapp/tier/resource/RedisResourceErrorTest.java
+++ application/jaxrs-redis-quarkus/src/test/java/webapp/tier/resource/RedisResourceErrorTest.java
@@ -1,17 +1,33 @@
 package webapp.tier.resource;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.mockito.Mockito.when;
 
+import java.security.NoSuchAlgorithmException;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
+import webapp.tier.service.RedisService;
 
 @QuarkusTest
 class RedisResourceErrorTest {
+	
+	@ConfigProperty(name = "common.message")
+	String message;
+
+	@InjectMock
+	RedisService svc;
+
 
 	@Test
-	void testPutError() {
+	void testPutError() throws NoSuchAlgorithmException {
+		when(svc.putMsg()).thenThrow(new RuntimeException());
 		given()
+				.accept(ContentType.JSON)
 				.when()
 				.contentType("application/json")
 				.post("/quarkus/redis/put")
@@ -21,22 +37,23 @@ class RedisResourceErrorTest {
 
 	@Test
 	void testGetError() {
+		when(svc.getMsgList()).thenThrow(new RuntimeException());
 		given()
+				.accept(ContentType.JSON)
 				.when()
 				.get("/quarkus/redis/get")
 				.then()
 				.statusCode(500);
 	}
-
+	
 	@Test
-	void testPublishError() {
+	void testDeleteError() {
+		when(svc.delete()).thenThrow(new RuntimeException());
 		given()
+				.accept(ContentType.JSON)
 				.when()
-				.contentType("application/json")
-				.post("/quarkus/redis/publish")
+				.delete("/quarkus/redis/delete")
 				.then()
 				.statusCode(500);
 	}
-
 }
-
