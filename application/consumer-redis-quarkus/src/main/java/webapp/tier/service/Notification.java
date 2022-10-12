91afefe393f28a9e16db1adf59d66797fba63388
yurak <devnulldevnull21@gmail.com>
2022-10-12 18:13:57 +0900

    delete document empty constructor #2379

--- application/consumer-redis-quarkus/src/main/java/webapp/tier/service/Notification.java
+++ application/consumer-redis-quarkus/src/main/java/webapp/tier/service/Notification.java
@@ -6,9 +6,6 @@ public final class Notification {
 
 	public String key;
 	public MsgBean msgbean;
-
-	public Notification() {
-	}
 	
 	public Notification(String key, MsgBean msgbean) {
 		this.key = key;
