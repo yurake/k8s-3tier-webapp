4975077b4f626a854ab1dd581e9a05a091bfb6da
yurak <devnulldevnull21@gmail.com>
2022-10-18 21:21:53 +0900

    create converter-rabbitmq-quarkus #2806

---
+++ application/converter-rabbitmq-quarkus/src/main/java/webapp/tier/service/RabbitmqConverter.java
@@ -0,0 +1,39 @@
+package webapp.tier.service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import webapp.tier.bean.MsgBean;
+import webapp.tier.util.MsgUtils;
+
+@ApplicationScoped
+public class RabbitmqConverter {
+
+	private final Logger logger = Logger.getLogger(this.getClass().getSimpleName());
+
+	@Inject
+	@RestClient
+	RabbitmqDeliverService deliversvc;
+
+	@ConfigProperty(name = "rabbitmq.split.key")
+	String splitkey;
+
+	@Incoming("message")
+	@Blocking
+	public void consume(String message) {
+		MsgBean msgbean = MsgUtils.splitBody(message, splitkey);
+		msgbean.setFullmsg("Received");
+		logger.log(Level.INFO, msgbean.getFullmsg());
+		String response = deliversvc.random();
+		logger.log(Level.INFO, "Called Random Publish: {0}", response);
+	}
+
+}
---
+++ application/converter-rabbitmq-quarkus/src/test/java/webapp/tier/service/RabbitmqConverterTest.java
@@ -0,0 +1,60 @@
+package webapp.tier.service;
+
+import static com.rabbitmq.client.BuiltinExchangeType.*;
+import static java.nio.charset.StandardCharsets.*;
+import static java.util.concurrent.TimeUnit.*;
+
+import java.util.Map;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+
+import io.quarkus.test.junit.QuarkusTest;
+import webapp.tier.bean.MsgBean;
+import webapp.tier.util.CreateId;
+import webapp.tier.util.MsgUtils;
+
+@QuarkusTest
+class RabbitmqConverterTest {
+
+	private static String message = ConfigProvider.getConfig().getValue("common.message",
+			String.class);
+	private static String splitkey = ConfigProvider.getConfig()
+			.getValue("rabbitmq.split.key", String.class);
+	private static String queueName = ConfigProvider.getConfig()
+			.getValue("mp.messaging.incoming.message.queue.name", String.class);
+	private static String routingkey = ConfigProvider.getConfig()
+			.getValue("mp.messaging.incoming.message.routing-keys", String.class);
+	private static String hostname = "localhost";
+	private static String exchangeName = "exchangemsg";
+
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void testSubscribe() throws Exception {
+		MsgBean msgbean = new MsgBean(CreateId.createid(), message, "Publish");
+		String body = MsgUtils.createBody(msgbean, splitkey);
+
+		Channel channel = getChannel();
+
+		channel.exchangeDeclare(exchangeName, TOPIC, true, false, Map.of());
+		channel.queueDeclare(queueName, true, false, false, Map.of());
+		channel.queueBind(queueName, exchangeName, routingkey);
+		AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
+				.contentType("text/plain")
+				.build();
+		channel.basicPublish(exchangeName, routingkey, props, body.getBytes(UTF_8));
+	}
+
+	Channel getChannel() throws Exception {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost(hostname);
+		connectionFactory.setChannelRpcTimeout((int) SECONDS.toMillis(3));
+		return connectionFactory.newConnection().createChannel();
+	}
+}
