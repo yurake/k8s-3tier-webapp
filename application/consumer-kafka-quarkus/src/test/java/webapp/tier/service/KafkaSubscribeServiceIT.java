5d604f70be369e5a2a743c5d264916ff715c9026
yurak <devnulldevnull21@gmail.com>
2023-02-13 12:08:29 +0900

    use InjectMock #3025

--- application/consumer-kafka-quarkus/src/test/java/webapp/tier/service/KafkaSubscribeServiceIT.java
+++ application/consumer-kafka-quarkus/src/test/java/webapp/tier/service/KafkaSubscribeServiceIT.java
@@ -1,7 +1,16 @@
 package webapp.tier.service;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 
 @QuarkusIntegrationTest
 class KafkaSubscribeServiceIT extends KafkaSubscribeServiceTest {
+	
+	@InjectMock
+	KafkaSubscribeService svc;
+
+	@Override
+	KafkaSubscribeService getSvc() {
+		return this.svc;
+	}
 }
